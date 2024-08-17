eef35f0642c471a6a1394c708e04dc7dcbbecf32
Alhadi <alhadi.rahman@cprvision.com>
2024-08-17 23:39:23 +0700

    fix: :lipstick: styling

--- components/RiskCalculator.js
+++ components/RiskCalculator.js
@@ -30,77 +30,89 @@ const RiskCalculator = () => {
 
     return (
         <div className="p-4 max-w-md mx-auto bg-white shadow-md rounded-lg">
-            <h2 className="text-lg font-bold mb-4">Risk Calculator</h2>
-            <div className="mb-4">
-                <label htmlFor="probability" className="block text-sm font-medium text-gray-700">Probability</label>
-                <select
-                    id="probability"
-                    value={selectedProbability}
-                    onChange={(e) => setSelectedProbability(e.target.value)}
-                    className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-                >
-                    <option value="">Select Probability</option>
-                    {probability.map(option => (
-                        <option key={option.id} value={option.weight}>
-                            {option.name}
-                        </option>
-                    ))}
-                </select>
-            </div>
-            <div className="mb-4">
-                <label htmlFor="exposure" className="block text-sm font-medium text-gray-700">Exposure</label>
-                <select
-                    id="exposure"
-                    value={selectedExposure}
-                    onChange={(e) => setSelectedExposure(e.target.value)}
-                    className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-                >
-                    <option value="">Select Exposure</option>
-                    {exposure.map(option => (
-                        <option key={option.id} value={option.weight}>
-                            {option.name}
-                        </option>
-                    ))}
-                </select>
-            </div>
-            <div className="mb-4">
-                <label htmlFor="consequence" className="block text-sm font-medium text-gray-700">Consequence</label>
-                <select
-                    id="consequence"
-                    value={selectedConsequence}
-                    onChange={(e) => {
-                        const selectedOption = consequence.find(option => option.id == e.target.value);
-                        setSelectedConsequence(e.target.value);
-                        setSelectedConsequenceWeight(selectedOption.weight);
-                        setAliasConsequenceSelected(selectedOption.alias)
-                    }}
-                    className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-                >
-                    <option value="">Select Consequence</option>
-                    {consequence.map(option => (
-                        <option key={option.id} value={option.id}>
-                            {option.name}
-                        </option>
-                    ))}
-                </select>
-                {aliasConsequenceSelected}
-            </div>
-            <button
-                onClick={handleSubmit}
-                className="ml-4 px-4 py-2 bg-green-600 text-white rounded-md hover:bg-green-700"
-            >
-                Submit
-            </button>
-            {riskLevel && (
-                <div className="mt-4 text-lg font-semibold">
-                    Risk Level: {riskLevel}
+            <h2 className="text-lg font-bold mb-4 text-blue-700">Risk Calculator</h2>
+            <div className='border border-blue-700 rounded-md p-3 mt-4'>
+                <div className="mb-4">
+                    <label htmlFor="probability" className="block text-sm font-medium text-gray-700">Probability</label>
+                    <select
+                        id="probability"
+                        value={selectedProbability}
+                        onChange={(e) => setSelectedProbability(e.target.value)}
+                        className="bg-blue-300 mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                    >
+                        <option value="">Select Probability</option>
+                        {probability.map(option => (
+                            <option key={option.id} value={option.weight}>
+                                {option.name}
+                            </option>
+                        ))}
+                    </select>
                 </div>
-            )}
-            {submissionStatus && (
-                <div className="mt-4 text-lg font-semibold">
-                    {submissionStatus}
+                <div className="mb-4">
+                    <label htmlFor="exposure" className="block text-sm font-medium text-gray-700">Exposure</label>
+                    <select
+                        id="exposure"
+                        value={selectedExposure}
+                        onChange={(e) => setSelectedExposure(e.target.value)}
+                        className="bg-blue-300 mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                    >
+                        <option value="">Select Exposure</option>
+                        {exposure.map(option => (
+                            <option key={option.id} value={option.weight}>
+                                {option.name}
+                            </option>
+                        ))}
+                    </select>
                 </div>
-            )}
+                <div className="mb-4">
+                    <label htmlFor="consequence" className="block text-sm font-medium text-gray-700">Consequence</label>
+                    <select
+                        id="consequence"
+                        value={selectedConsequence}
+                        onChange={(e) => {
+                            const selectedOption = consequence.find(option => option.id == e.target.value);
+                            setSelectedConsequence(e.target.value);
+                            setSelectedConsequenceWeight(selectedOption.weight);
+                            setAliasConsequenceSelected(selectedOption.alias)
+                        }}
+                        className="bg-blue-300 mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                    >
+                        <option value="">Select Consequence</option>
+                        {consequence.map(option => (
+                            <option key={option.id} value={option.id}>
+                                {option.name} ( {option.alias} )
+                            </option>
+                        ))}
+                    </select>
+                    {/* <span className='text-black'>{aliasConsequenceSelected}</span> */}
+
+                </div>
+                <button
+                    onClick={handleSubmit}
+                    className="bg-blue-300 ml-4 px-4 py-2  text-white rounded-md hover:bg-blue-700"
+                >
+                    Calculate
+                </button>
+
+            </div>
+            {
+                (riskLevel || submissionStatus) &&
+                <div className='border border-blue-700 rounded-md p-1 mt-4'>
+                    <h2 className='text-blue-700 font-bold'>Result</h2>
+                    {riskLevel && (
+                        <div className="mt-4 text-lg font-semibold text-gray-700">
+                            Risk Level: {riskLevel}
+                        </div>
+                    )}
+                    {submissionStatus && (
+                        <div className="mt-4 text-lg font-semibold text-gray-700">
+                            {submissionStatus}
+                        </div>
+                    )}
+                </div>
+            }
+
+
         </div>
     );
 };
